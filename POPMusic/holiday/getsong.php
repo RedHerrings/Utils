5f9f8f204e7f019f6008d79afecf007660ce367e
Hialan Liu <hialan.liu@gmail.com>
2013-05-01 22:51:11 +0800

    add apc cache

--- POPMusic/holiday/getsong.php
+++ POPMusic/holiday/getsong.php
@@ -1,6 +1,10 @@
 <?php
 ini_set( 'default_charset', 'UTF-8' );
 
+define('HOLIDAY_CACHE_PREFIX', 'hd_cache_prefix_');
+define('YOUTUBE_CACHE_PREFIX', 'yt_caceh_prefix_');
+define('SONG_CACHE_SECOND', 500);
+
 require_once __DIR__ . '/holidayParser.inc';
 require_once __DIR__ . '/YQL.inc';
 
@@ -14,7 +18,7 @@ $menu = array(
 	'jt' => '東洋點播',
 );
 
-$kind = $_GET['kind'];
+$kind = filter_input(INPUT_GET, 'kind', FILTER_SANITIZE_STRING); 
 if(empty($kind))
 {
 	$kind = "nc";
@@ -33,13 +37,38 @@ foreach($menu as $key => $str)
 
 echo "<hr>\n";
 
+// parse holiday
+$holiday_cache_key = HOLIDAY_CACHE_PREFIX . $kind;
+if(apc_exists($holiday_cache_key))
+{
+	$total_songs = apc_fetch($holiday_cache_key);
+}
+else
+{
+	$total_songs = get_songs($kind);
+	apc_store( $holiday_cache_key, $total_songs, SONG_CACHE_SECOND);
+}
+
+// default: first song
+if(empty($song_name))
+{
+  $song = $total_songs[0];
+  $song_name = "{$song['singer']} {$song['song']}";
+}
+
 if(!empty($song_name))
 {
-
-$song_list = SearchYoutube("$song_name MV");
-
-$video_id = $song_list['query']['results']['video'][0]['id'];
-
+  $song_hash_key = YOUTUBE_CACHE_PREFIX . md5($song_name);
+  if(apc_exists($song_hash_key))
+  {
+    $video_id = apc_fetch($song_hash_key);
+  }
+  else
+  {
+    $song_list = SearchYoutube("$song_name MV");
+    $video_id = $song_list['query']['results']['video'][0]['id'];
+    apc_store($song_hash_key, $video_id, SONG_CACHE_SECOND); 
+  }
 ?>
 <H1><?php echo $song_name; ?></H1>
 
@@ -57,7 +86,11 @@ $video_id = $song_list['query']['results']['video'][0]['id'];
 
     function onYouTubePlayerReady(playerId) {
       var videoId = "<?php echo $video_id; ?>";
+      play(videoId);
+    }
 
+    function play(videoId)
+    {
       ytplayer = document.getElementById("myytplayer");
       ytplayer.loadVideoById(videoId, 0, "large");
       ytplayer.playVideo();
@@ -67,7 +100,6 @@ $video_id = $song_list['query']['results']['video'][0]['id'];
 <?php
 }
 
-$total_songs = get_songs($kind);
 
 function renderHTML(array $songs)
 {
